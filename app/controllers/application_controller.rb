64d59c2ca4c2d50faf73ad5f377c5da7369060d8
faresabe <faresabenet9@gmail.com>
2025-05-23 21:48:04 +0300

    add sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :track_last_visit
+  before_action :track_page_visits
+
+  private
+  
+  def track_page_visits
+    session[:page_visits] ||= {}
+    path = request.path
+    session[:page_visits][path] = session[:page_visits].fetch(path, 0) + 1
+  
+    session[:total_visits] ||= 0
+    session[:total_visits] += 1
+  end
+  
+private
+
+def track_last_visit
+  @last_visit = session[:last_visit]
+  session[:last_visit] = Time.current
+end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+    def time_since_last_visit
+        return "This is your first visit." unless @last_visit
+      
+        distance = Time.current - Time.parse(@last_visit.to_s)
+        if distance < 60
+          "Last visited: #{distance.to_i} seconds ago"
+        elsif distance < 3600
+          "Last visited: #{(distance / 60).to_i} minutes ago"
+        elsif distance < 86400
+          "Last visited: #{(distance / 3600).to_i} hours ago"
+        else
+          "Last visited: #{(distance / 86400).to_i} days ago"
+        end
+    end
+    def page_visit_count
+        path = request.path
+        session[:page_visits][path] || 0
+    end
+      
+      def total_visit_count
+        session[:total_visits] || 0
+      end
+      def time_based_greeting
+        hour = Time.current.hour
+        case hour
+        when 5..11
+          "Good morning!"
+        when 12..16
+          "Good afternoon!"
+        when 17..20
+          "Good evening!"
+        else
+          "Good night!"
+        end
+      end
+      
 end
